ee547d14ff8619ab6ebf8927291017f40121350e
Guilherme Pejon <guilpejon@gmail.com>
2019-07-09 23:15:07 -0300

    Finished camera first logic

--- src/pages/camera/index.js
+++ src/pages/camera/index.js
@@ -1,18 +1,107 @@
-import React from 'react'
+'use strict';
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import api from '../../services/api';
+import { NavigationActions } from 'react-navigation';
 
-import {
-  Text ,
-  View
-} from 'react-native'
+export default class Camera extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
-const Camera = ({ navigation }) => (
-  <View>
-    <Text>OLA</Text>
-  </View>
-);
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true, doNotSave: true };
+      const data = await this.camera.takePictureAsync(options);
+      const response = await api.post('/registries', {
+        image: data.base64
+      });
+      console.log(response)
+      if (response.status === 200) {
+        const backAction = NavigationActions.back({
+          key: 'Main',
+        });
+        this.props.navigation.dispatch(backAction);
+      }
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+// AppRegistry.registerComponent('Camera', () => Camera);
+// export default Camera;
+
+
+
+
+// import React from 'react'
+
+// import {
+//   Text ,
+//   View
+// } from 'react-native'
+
+// const Camera = ({ navigation }) => (
+//   <View>
+//     <Text>OLA</Text>
+//   </View>
+// );
 
 // Registry.navigationOptions = ({ navigation }) => ({
 //   title: navigation.state.params.registry.createdAt
 // });
 
-export default Camera;
+// export default Camera;
